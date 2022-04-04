57d0610b00628aa69a1032e7136651ae2c86d2c1
benniho03 <ben.holderle.21@lehre.mosbach.dhbw.de>
2022-04-04 20:46:24 +0200

    added first ui and fetch logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-const APIkey :string = "1a4f82d943f93178acc0f7676af43556"
-let cityName :string = "Stuttgart";
+const apiKey = "1a4f82d943f93178acc0f7676af43556"
+const cityName :string = "Michelfeld";
 
-const fetchURL :string = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`
+const btn = document.getElementById("EnterCity") as HTMLButtonElement;
+const city = document.querySelector("#location") as HTMLParagraphElement;
+const deg = document.querySelector("#degrees") as HTMLParagraphElement
 
-fetch(fetchURL)
-  .then(res => res.json)
-  .catch(err => console.log(err))
+const fetchURL :string = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+const kelvinCalc :number= -273
+
+function loadWeather(){
+  return fetch(fetchURL)
+  .then(response => response.json())
+  .then(data => data)
+}
+
+function displayWeather () {
+  loadWeather()
+    .then(data => {
+      deg.textContent = (Math.floor(kelvinCalc + data.main.temp)).toString()
+      city.textContent = (data.name)
+    })
+    .catch(err => err)
+}
+
+btn.addEventListener('click', displayWeather)

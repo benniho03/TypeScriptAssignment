01a7c7de97a15a5733bf297d57f9126054fa4524
benniho03 <ben.holderle.21@lehre.mosbach.dhbw.de>
2022-04-05 21:52:05 +0200

    modified design

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import { createConditional } from "../node_modules/typescript/lib/typescript";
-
 const btn = document.getElementById("EnterCity") as HTMLButtonElement;
 const city = document.querySelector("#location") as HTMLParagraphElement;
 const deg = document.querySelector("#degrees") as HTMLParagraphElement;
@@ -8,17 +6,20 @@ const weatherIcon = document.querySelector("#icon") as HTMLParagraphElement;
 const cityInput = document.querySelector("#cityInput") as HTMLInputElement;
 
 const apiKey :string = "1a4f82d943f93178acc0f7676af43556"
-let cityName :string = "";
+let cityName :string = "London";
 let fetchURL :string = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
 
 btn.addEventListener('click', displayWeather)
 
 const kelvinCalc :number= -273;
 
+//Abfrage mit Wert New York
+displayWeather();
+
 function displayWeather () {
   loadWeather()
     .then(data => {
-      deg.textContent = (Math.floor(kelvinCalc + data.main.temp)).toString() +"°"
+      deg.textContent = (Math.floor(kelvinCalc + data.main.temp)).toString() +"°C"
       city.textContent = `${data.name}, ${data.sys.country}`
       cond.textContent = data.weather[0].main
       console.log(data)
@@ -27,8 +28,30 @@ function displayWeather () {
 }
 
 function loadWeather(){
-  cityName = cityInput.value;
+  if(!cityInput.value){
+    cityName = "New York"
+  } else {
+    cityName = cityInput.value;
+  }
+
   return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
   .then(response => response.json())
   .then(data => data)
 }
+
+// function getUserPosition () {
+//   navigator.geolocation.getCurrentPosition(success, error);
+// }
+
+// const success = (position :object) => position.coords.latitude
+// function successOld () {
+//   const lon = position.coords.longitude
+//   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+//     .then(res => res)
+//     .then(data => data)
+//     .catch(err => err)
+// }
+
+// function error () {
+//   alert("Schade")
+// }

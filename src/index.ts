cc411613439c4c3a66ba2d2d169c8a436f3de37c
benniho03 <ben.holderle.21@lehre.mosbach.dhbw.de>
2022-04-08 15:09:51 +0200

    extracted ui actions

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,48 @@
-import { btn, deg, city, cond, flag, icon, cityInput } from "./dom-utils";
+import { btn, deg, city, flag, icon, cityInput, minDeg, maxDeg, kelvinCalc } from "./dom-utils";
+import { updateUI } from "./ui-actions";
 
 
 const apiKey :string = "1a4f82d943f93178acc0f7676af43556"
-let cityName :string = "Michelfeld";
+let cityName :string = "Berlin";
 let fetchURL :string = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
 
 btn.addEventListener('click', displayWeather)
 
-const kelvinCalc :number= -273;
+navigator.geolocation.getCurrentPosition((position) => {
+  const lat :number= position.coords.latitude
+  const lon :number= position.coords.longitude
 
-//Abfrage mit Wert New York
-displayWeather();
+  loadWeatherCoords(lat, lon)
+})
 
 function displayWeather () {
   loadWeather()
-    .then(data => {
-      deg.textContent = (Math.floor(kelvinCalc + data.main.temp)).toString() +"°C"
-      city.textContent = `${data.name}`
-      cond.textContent = data.weather[0].main
-      flag.src = `https://flagcdn.com/120x90/${(data.sys.country).toLowerCase()}.png`
-      flag.srcset = `https://flagcdn.com/240x180/${(data.sys.country.toLowerCase())}.png 2x`
-      icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-      console.log(data)
-      console.log(data.weather[0].icon)
-    })
+    .then(data => updateUI(data))
     .catch(err => console.log(err))
 }
 
 
-function loadWeather(){
-  if(!cityInput.value){
-    cityName = "Michelfeld"
-  } else {
-    cityName = cityInput.value;
-  }
 
+function loadWeather() {
+  setDefaultCity()
   return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
   .then(response => response.json())
   .then(data => data)
 }
+
+function loadWeatherCoords(lat :number, lon :number){
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+function setDefaultCity(){
+  if(!cityInput.value){
+    cityName = "Berlin"
+  } else {
+    cityName = cityInput.value;
+  }
+}
+
+//Abfrage mit Wert Berlin
+displayWeather();
